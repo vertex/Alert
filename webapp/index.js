340a690652a667fee45c2e1a1f20dad889d2392d
Bryon Keck <bkeck@paypal.com>
2017-08-09 10:49:08 -0700

    add prettier

--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,12 @@
 /* @flow */
-import "./scss/app.scss"
+import "./scss/app.scss";
 import * as firebase from "firebase";
 
 var config = {
-    apiKey: "AIzaSyDqXCPkdJxLwx8jBGaJYxRQ7eOLx7N0Fhk",
-    authDomain: "project-6329594795012513265.firebaseapp.com",
-    databaseURL: "https://project-6329594795012513265.firebaseio.com/",
-    storageBucket: "gs://project-6329594795012513265.appspot.com",
+  apiKey: "AIzaSyDqXCPkdJxLwx8jBGaJYxRQ7eOLx7N0Fhk",
+  authDomain: "project-6329594795012513265.firebaseapp.com",
+  databaseURL: "https://project-6329594795012513265.firebaseio.com",
+  storageBucket: "gs://project-6329594795012513265.appspot.com"
 };
 firebase.initializeApp(config);
-//firebase.auth();?
 console.log(firebase.auth());
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
 var path = require("path");
-var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
+var FlowBabelWebpackPlugin = require("flow-babel-webpack-plugin");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-            test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
         ]
-    },
-    devtool: "source-map",
-    entry: "./webapp/index.js",
-    devServer: {
-        contentBase: "./public/"
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "app.js"
-    },
-    plugins: [
-        new FlowBabelWebpackPlugin()
+      }
     ]
-}
+  },
+  devtool: "source-map",
+  entry: "./webapp/index.js",
+  devServer: {
+    contentBase: "./public/"
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app.js"
+  },
+  plugins: [new FlowBabelWebpackPlugin()]
+};
